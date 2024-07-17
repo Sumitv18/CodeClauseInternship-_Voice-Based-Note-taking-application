38eb9588bf91c3a57492b9f8480a9bffd7d9315b
Sumitv18 <sumitverma945466@gmail.com>
2024-07-17 15:17:40 +0530

    created flutter project

---
+++ lib/consts.dart
@@ -0,0 +1,3 @@
+// ignore: non_constant_identifier_names
+String TTS_INPUT =
+    "My name is sumit verma I am a BTech second year student with electronics and communication branch My intrest of area is robotics and android development and my hobbies are playing cricket and listening the music and i am from up shahjahanpur";
